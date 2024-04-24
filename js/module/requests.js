3c633b73e3cb6aa9b5e28b9cf2adf65b75c088fe
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-04-24 13:12:04 -0500

    feat: :sparkles: Ejercicio 10

--- js/module/requests.js
+++ js/module/requests.js
@@ -3,9 +3,9 @@
 // export const getAll = async() => {
 //     let res = await fetch("http://localhost:5508/requests");
 //     let data = await res.json();
-//     let estadosUnicos = new Set(); 
+//     let estadosUnicos = new Set();
 //     data.forEach(val => {
-//         estadosUnicos.add(val.status); 
+//         estadosUnicos.add(val.status);
 //     });
 //     let combi = [...estadosUnicos].map(estado => ({ estado }))
 
@@ -24,14 +24,14 @@
 export const getAllStatusOrder = async () => {
     const res = await fetch("http://localhost:5508/requests");
     const data = await res.json();
-    
+
     let dataUpdate = [];
     for (let item of data) {
         if (!dataUpdate.includes(item.status)) {
             dataUpdate.push(item.status);
         }
     }
-    
+
     let combi = dataUpdate.map(estado => ({ estado }));
     return combi;
 }
@@ -55,17 +55,53 @@ export const getOrdersClientsExpectedDateAndDelivery = async() => {
 
 // 10. Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
 
+// OPCION 1.
+
 export const getAll = async() => {
-    let res = await fetch("http://localhost:5508/requests");
+    let res = await fetch("http://localhost:5508/requests?status=Entregado");
     let data = await res.json();
     let dataUpdate = [];
     data.forEach(val => {
-        dataUpdate.push({
-            pedido: val.code_request,
-            cliente: val.code_client,
-            espera: val.date_wait,
-            retraso: val.date_delivery
-        })
+        if (val.date_delivery == null) {
+            dataUpdate.push(
+                val.code_request,
+                val.code_client,
+                val.date_wait,
+                val.date_delivery
+            )
+            } else {
+                let delivery = val.date_delivery.split('-')[2];
+                let wait = val.date_wait.split('-')[2];
+                if ( Number( wait) - Number(delivery) >= 2){
+                    dataUpdate.push({
+                        pedido: val.code_request,
+                        codigo_cliente: val.code_client,
+                        tiempo_espera: val.date_wait,
+                        tiempo_llegada: val.date_delivery
+
+                    }
+                    )
+                }
+            }
     })
     return dataUpdate
 }
+
+// OPCION 2.
+
+// export const getAll = async() => {
+//     let res = await fetch("http://localhost:5508/requests");
+//     let data = await res.json();
+//     let dataUpdate = data.filter(val => (val.date_wait > val.date_delivery))
+//     .map(val => {
+//          return {
+//              pedido: val.code_request,
+//              codigo_cliente: val.code_client,
+//              tiempo_espera: val.date_wait,
+//              tiempo_llegada: val.date_delivery
+//          }
+
+//     });
+
+//     return dataUpdate
+// }
