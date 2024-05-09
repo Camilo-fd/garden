5e4bcd2c8b86f255eac6377494e07e275e2328ab
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-05-09 12:53:07 -0500

    feat: :sparkles: Consulta #2 WebComponents

--- js/component/myCard.js
+++ js/component/myCard.js
@@ -1,5 +1,6 @@
 import { 
     getAllOfficeCodeAndCity,
+    getAllOfficesFromSpainCityAndMovil
 } from '../module/offices.js'
 
 export class Mycard extends HTMLElement {
@@ -31,11 +32,32 @@ export class Mycard extends HTMLElement {
         })
     }
 
+    async getAllOfficesFromSpainCityAndMovilDesing() {
+        let data = await getAllOfficesFromSpainCityAndMovil()
+        console.log(JSON.stringify(data, null, 4));
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /*html*/ `
+                <div class="report__card">
+                    <div class="card__title">
+                        <div>CLIENTES</div>
+                    </div>
+                    <div class="card__body">
+                        <div class="body__marck">
+                        <p><b>telefono: </b>${val.telefono}</p>
+                        <p><b>Ciudad: </b>${val.ciudad}</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        })
+    }
+
     static get observedAttributes() {
         return ["logic"];
     }
 
     attributeChangedCallback(name, old, now) {
         if(name=="logic" && now=="office_1") this.getAllOfficeCodeAndCityDesing()
+        if(name=="logic" && now=="office_2") this.getAllOfficesFromSpainCityAndMovilDesing()
     }
 }
